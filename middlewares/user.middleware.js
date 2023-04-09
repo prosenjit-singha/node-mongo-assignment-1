f07261a5a674443867ca722c6ecb7b72d25907f6
PJAheibam <prosenjitsingha28@gmail.com>
2023-04-09 17:05:42 +0600

    Middlewares and user.json created

---
+++ middlewares/user.middleware.js
@@ -0,0 +1,4 @@
+module.exports.validateId = () => {};
+
+// validate the body and check if all the required properties are present in the body
+module.exports.validateObj = () => {};
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/user.controller");
+const middleware = require("../middlewares/user.middleware");
 
 router.get("/user/random", controller.getARandomUser);
 
 router.get("/user/all", controller.getTheAllUsers);
 
-router.put("/user/save", controller.createAnUser);
+router.put("/user/save", middleware.validateObj, controller.createAnUser);
 
 router.patch("/user/update", controller.updateAnUser);
 
